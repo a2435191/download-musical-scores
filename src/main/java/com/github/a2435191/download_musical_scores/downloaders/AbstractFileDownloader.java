9a115d7b9b26e3890f9ca3ebac60debe5886fd9c
Will Bradley <7458201@isd535.org>
2022-04-30 00:26:16 -0500

    Make AbstractFileDownloader more extensible by taking in URI predicates instead of hard-coding.

--- src/main/java/com/github/a2435191/download_musical_scores/downloaders/AbstractFileDownloader.java
+++ src/main/java/com/github/a2435191/download_musical_scores/downloaders/AbstractFileDownloader.java
@@ -8,7 +8,9 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.Locale;
+import java.security.GeneralSecurityException;
+import java.util.*;
+import java.util.function.BiPredicate;
 
 public abstract class AbstractFileDownloader {
 
@@ -16,24 +18,51 @@ public abstract class AbstractFileDownloader {
     public abstract @NotNull AbstractFileNode getFileTreeRoot(@NotNull String url) throws IOException;
 
     public static class DownloaderManager {
-        private final @NotNull WeTransferDownloader weTransferDownloader;
-        private final @NotNull GoogleDriveDownloader googleDriveDownloader;
-        private final @NotNull SendspaceDownloader sendspaceDownloader;
-        private final @NotNull MegaDownloader megaDownloader;
-        private final @NotNull StackStorageDownloader stackStorageDownloader;
+        public final Map<BiPredicate<@NotNull URI, @NotNull String>, @NotNull AbstractFileDownloader> downloaders
+            = new HashMap<>();
 
-        public DownloaderManager(
-            @NotNull WeTransferDownloader weTransferDownloader,
-            @NotNull GoogleDriveDownloader googleDriveDownloader,
-            @NotNull SendspaceDownloader sendspaceDownloader,
-            @NotNull MegaDownloader megaDownloader,
-            @NotNull StackStorageDownloader stackStorageDownloader
-        ) {
-            this.weTransferDownloader = weTransferDownloader;
-            this.googleDriveDownloader = googleDriveDownloader;
-            this.sendspaceDownloader = sendspaceDownloader;
-            this.megaDownloader = megaDownloader;
-            this.stackStorageDownloader = stackStorageDownloader;
+
+        public void put(@NotNull AbstractFileDownloader downloader, @NotNull String domainName) {
+            downloaders.put((uri, host) -> host.equals(domainName), downloader);
+        }
+
+        public void put(@NotNull AbstractFileDownloader downloader, @NotNull String... domainNames) {
+            HashSet<String> domainNamesSet = new HashSet<>(List.of(domainNames));
+            downloaders.put((uri, host) -> domainNamesSet.contains(host), downloader);
+        }
+
+
+        @Contract(value = "_ -> this", mutates = "this")
+        public @NotNull DownloaderManager addGoogleDriveDownloader(int timeoutSeconds) throws GeneralSecurityException, IOException {
+            this.put(new GoogleDriveDownloader(timeoutSeconds), "drive.google.com");
+            return this;
+        }
+
+        @Contract(value = "_ -> this", mutates = "this")
+        public @NotNull DownloaderManager addSendspaceDownloader(int timeoutSeconds) {
+            this.put(new SendspaceDownloader(timeoutSeconds), "sendspace.com");
+            return this;
+        }
+
+        @Contract(value = "_, _ -> this", mutates = "this")
+        public @NotNull DownloaderManager addMegaDownloader(@NotNull String username, @NotNull String password) {
+            this.put(new MegaDownloader(username, password), "mega.nz", "mega.co.nz");
+            return this;
+        }
+
+        @Contract(value = "_ -> this", mutates = "this")
+        public @NotNull DownloaderManager addWeTransferDownloader(int timeoutSeconds) {
+            this.put(new WeTransferDownloader(timeoutSeconds), "we.tl", "wetransfer.com");
+            return this;
+        }
+
+        @Contract(value = "_ -> this", mutates = "this")
+        public @NotNull DownloaderManager addStackStorageDownloader(int timeoutSeconds) {
+            this.downloaders.put(
+                (uri, host) -> host.contains("stackstorage.com"),
+                new StackStorageDownloader(60)
+            );
+            return this;
         }
 
         @Contract(pure = true)
@@ -42,16 +71,15 @@ public abstract class AbstractFileDownloader {
             if (host.startsWith("www.")) {
                 host = host.substring("www.".length());
             }
-            if (host.contains("stackstorage.com")) {
-                return this.stackStorageDownloader;
+            synchronized (this.downloaders) {
+                for (var pair : this.downloaders.entrySet()) {
+                    BiPredicate<@NotNull URI, @NotNull String> predicate = pair.getKey();
+                    if (predicate.test(url, host)) {
+                        return pair.getValue();
+                    }
+                }
             }
-            return switch (host) {
-                //case "we.tl", "wetransfer.com" -> this.weTransferDownloader;
-                case "drive.google.com" -> this.googleDriveDownloader;
-                case "sendspace.com" -> this.sendspaceDownloader;
-                //case "mega.nz" -> this.megaDownloader;
-                default -> null;
-            };
+            return null;
         }
 
     }
