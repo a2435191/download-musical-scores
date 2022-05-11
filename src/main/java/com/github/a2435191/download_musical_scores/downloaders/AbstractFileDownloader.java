e2c987b021e4e2825d8428499ca636a36736fb3f
Will Bradley <7458201@isd535.org>
2022-05-11 01:23:07 -0500

    replace references to the same object in AbstractFileDownloader with lambda constructors

--- src/main/java/com/github/a2435191/download_musical_scores/downloaders/AbstractFileDownloader.java
+++ src/main/java/com/github/a2435191/download_musical_scores/downloaders/AbstractFileDownloader.java
@@ -6,27 +6,34 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.concurrent.ThreadSafe;
 import java.io.IOException;
 import java.net.URI;
 import java.security.GeneralSecurityException;
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiPredicate;
+import java.util.function.Supplier;
 
 public abstract class AbstractFileDownloader {
 
 
     public abstract @NotNull AbstractFileNode getFileTreeRoot(@NotNull String url) throws IOException;
 
+    @ThreadSafe
     public static class DownloaderManager {
-        public final Map<BiPredicate<@NotNull URI, @NotNull String>, @NotNull AbstractFileDownloader> downloaders
-            = new HashMap<>();
+        public final Map<BiPredicate<@NotNull URI, @NotNull String>, @NotNull Supplier<@NotNull AbstractFileDownloader>> downloaders
+            = new ConcurrentHashMap<>();
 
 
-        public void put(@NotNull AbstractFileDownloader downloader, @NotNull String domainName) {
+        public void put(@NotNull Supplier<@NotNull AbstractFileDownloader> downloader, @NotNull String domainName) {
             downloaders.put((uri, host) -> host.equals(domainName), downloader);
         }
 
-        public void put(@NotNull AbstractFileDownloader downloader, @NotNull String... domainNames) {
+        public void put(@NotNull Supplier<@NotNull AbstractFileDownloader> downloader, @NotNull String... domainNames) {
             HashSet<String> domainNamesSet = new HashSet<>(List.of(domainNames));
             downloaders.put((uri, host) -> domainNamesSet.contains(host), downloader);
         }
@@ -34,25 +41,31 @@ public abstract class AbstractFileDownloader {
 
         @Contract(value = "_ -> this", mutates = "this")
         public @NotNull DownloaderManager addGoogleDriveDownloader(int timeoutSeconds) throws GeneralSecurityException, IOException {
-            this.put(new GoogleDriveDownloader(timeoutSeconds), "drive.google.com");
+            this.put(() -> {
+                try {
+                    return new GoogleDriveDownloader(timeoutSeconds);
+                } catch (GeneralSecurityException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }, "drive.google.com");
             return this;
         }
 
         @Contract(value = "_ -> this", mutates = "this")
         public @NotNull DownloaderManager addSendspaceDownloader(int timeoutSeconds) {
-            this.put(new SendspaceDownloader(timeoutSeconds), "sendspace.com");
+            this.put(() -> new SendspaceDownloader(timeoutSeconds), "sendspace.com");
             return this;
         }
 
-        @Contract(value = "_, _ -> this", mutates = "this")
-        public @NotNull DownloaderManager addMegaDownloader(@NotNull String username, @NotNull String password) {
-            this.put(new MegaDownloader(username, password), "mega.nz", "mega.co.nz");
+        @Contract(value = "-> this", mutates = "this")
+        public @NotNull DownloaderManager addMegaDownloader() {
+            this.put(MegaDownloader::new, "mega.nz", "mega.co.nz");
             return this;
         }
 
         @Contract(value = "_ -> this", mutates = "this")
         public @NotNull DownloaderManager addWeTransferDownloader(int timeoutSeconds) {
-            this.put(new WeTransferDownloader(timeoutSeconds), "we.tl", "wetransfer.com");
+            this.put(() -> new WeTransferDownloader(timeoutSeconds), "we.tl", "wetransfer.com");
             return this;
         }
 
@@ -60,7 +73,7 @@ public abstract class AbstractFileDownloader {
         public @NotNull DownloaderManager addStackStorageDownloader(int timeoutSeconds) {
             this.downloaders.put(
                 (uri, host) -> host.contains("stackstorage.com"),
-                new StackStorageDownloader(60)
+                () -> new StackStorageDownloader(60)
             );
             return this;
         }
@@ -71,14 +84,13 @@ public abstract class AbstractFileDownloader {
             if (host.startsWith("www.")) {
                 host = host.substring("www.".length());
             }
-            synchronized (this.downloaders) {
-                for (var pair : this.downloaders.entrySet()) {
-                    BiPredicate<@NotNull URI, @NotNull String> predicate = pair.getKey();
-                    if (predicate.test(url, host)) {
-                        return pair.getValue();
-                    }
+            for (var pair : this.downloaders.entrySet()) {
+                BiPredicate<@NotNull URI, @NotNull String> predicate = pair.getKey();
+                if (predicate.test(url, host)) {
+                    return pair.getValue().get();
                 }
             }
+
             return null;
         }
 
