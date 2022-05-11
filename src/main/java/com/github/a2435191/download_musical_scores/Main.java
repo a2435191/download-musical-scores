695cd0bcbd7a4ed49f72764e001a3804da742c87
Will Bradley <7458201@isd535.org>
2022-05-11 01:21:18 -0500

    Restructure main method (this time with streams)

--- src/main/java/com/github/a2435191/download_musical_scores/Main.java
+++ src/main/java/com/github/a2435191/download_musical_scores/Main.java
@@ -4,16 +4,15 @@ import com.github.a2435191.download_musical_scores.reddit.RedditPostInfo;
 import com.github.a2435191.download_musical_scores.reddit.SubredditStream;
 import com.github.a2435191.download_musical_scores.util.BadRequestStatusException;
 import com.github.a2435191.download_musical_scores.util.FileUtils;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Main {
 
@@ -28,59 +27,82 @@ public final class Main {
         }
     }
 
-    public static void main(String[] args) {
+    private static void downloadAll() {
         final Path DOWNLOAD_DIR = Path.of("downloads");
-        List<CompletableFuture<Void>> futures = new ArrayList<>();
+
         MusicalScoresDownloader downloader = new MusicalScoresDownloader();
         SubredditStream stream = new SubredditStream();
-        while (!stream.isDone()) {
-            System.out.println("HERE1");
-            try {
-                RedditPostInfo[] dataArray = stream.getNextPostData();
-                for (RedditPostInfo info : dataArray) {
-                    for (String url : info.scoreURLs()) {
-                        String escapedTitle = info.title().replace("/", "|");
-                        if (escapedTitle.toUpperCase().startsWith(SUBMISSION_PREFIX)) {
-                            escapedTitle = escapedTitle
-                                .substring(SUBMISSION_PREFIX.length())
-                                .stripLeading();
-                        }
 
-                        Path targetPath = DOWNLOAD_DIR.resolve(escapedTitle);
-
-                        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-                            try {
-                                System.out.println("Downloading url " + url);
-                                downloader.download(url, targetPath);
-                                System.out.println(url + " done at " + targetPath);
-                            } catch (IOException | RuntimeException e) {
-                                System.err.println("error downloading " + url);
-                                e.printStackTrace(System.err);
-                                FileUtils.delete(targetPath.toFile());
-                            }
-                            System.out.println("done!");
-                        });
-                        futures.add(future);
+        Stream<@NotNull RedditPostInfo> infoStream = Stream.generate(() -> {
+                try {
+                    System.out.println("got");
+                    return stream.getNextPostData();
+                } catch (BadRequestStatusException badRequestStatusException) {
+                    if (badRequestStatusException.getResponse().statusCode() == 429) { // too many requests
+                        System.out.println("WAITING");
+                        sleep(60_000);
                     }
+                    badRequestStatusException.printStackTrace(System.err);
+                    return null;
                 }
-            } catch (BadRequestStatusException badRequestStatusException) {
-                if (badRequestStatusException.getResponse().statusCode() == 429) { // too many requests
-                    System.out.println("WAITING");
-                    sleep(60_000);
+            })
+            .takeWhile($ -> !stream.isDone())
+            .filter(Objects::nonNull)
+            .flatMap(Stream::of).limit(50);
+
+        Stream<@NotNull CompletableFuture<Void>> futures = infoStream.map(info -> {
+            List<CompletableFuture<Void>> tmpFutures = new ArrayList<>();
+            for (String url : info.scoreURLs()) {
+                String escapedTitle = info.title().replace("/", "|");
+                if (escapedTitle.toUpperCase().startsWith(SUBMISSION_PREFIX)) {
+                    escapedTitle = escapedTitle
+                        .substring(SUBMISSION_PREFIX.length())
+                        .stripLeading();
                 }
-                badRequestStatusException.printStackTrace(System.err);
+
+                Path targetPath = DOWNLOAD_DIR.resolve(escapedTitle);
+
+
+                System.out.println("downloading " + url);
+                CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                    try {
+                        System.out.println("Downloading url " + url);
+                        downloader.download(url, targetPath);
+                        System.out.println(url + " done at " + targetPath);
+                    } catch (FileAlreadyExistsException e) {
+                        e.printStackTrace(System.err);
+                    } catch (IOException | RuntimeException e) {
+                        StringBuilder builder = new StringBuilder("error downloading " + url + ": \n");
+                        for (StackTraceElement elem : e.getStackTrace()) {
+                            builder.append(elem.toString());
+                        }
+                        String stackTrace = builder.toString();
+
+                        System.err.println(stackTrace);
+                        //e.printStackTrace(System.err);
+                        FileUtils.delete(targetPath.toFile());
+                        return;
+                    }
+                    System.out.println("done!");
+
+                });
+                tmpFutures.add(future);
             }
-        }
+            return tmpFutures;
+        })
+        .flatMap(Collection::stream);
 
-        System.out.println("HERE2");
 
-        for (CompletableFuture<Void> future : futures) {
+        List<@NotNull CompletableFuture<Void>> futuresList = futures.toList();
+        for (@NotNull CompletableFuture<Void> future : futuresList) {
+            System.out.println("joining");
             future.join();
         }
-
-
     }
 
+    public static void main(String[] args) {
+        downloadAll();
+    }
 
 
 
