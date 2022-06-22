e42a2f7367517aa14c76f9a67c352c35aa8f94e7
Will Bradley <derivativedude123@gmail.com>
2022-06-21 20:22:29 -0500

    add functionality to recursively zip

--- src/main/java/com/github/a2435191/download_musical_scores/util/FileUtils.java
+++ src/main/java/com/github/a2435191/download_musical_scores/util/FileUtils.java
@@ -2,39 +2,92 @@ package com.github.a2435191.download_musical_scores.util;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Stack;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public final class FileUtils {
+    private static final int ZIP_BUFSIZE = 2048;
+
     private FileUtils() {
+    }
+
+    public static void delete(@NotNull Path folder) throws IOException {
+        Files.walkFileTree(folder, new FileVisitor<>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                throw exc;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                if (exc != null) {
+                    throw exc;
+                }
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    public static void zipDirectory(@NotNull Path directory, @NotNull Path out) throws IOException {
+        try (
+            FileOutputStream outputStream = new FileOutputStream(out.toString());
+            ZipOutputStream zipOut = new ZipOutputStream(outputStream)
+        ) {
+            Files.walkFileTree(directory, (SimpleFileVisitor) (Path file, BasicFileAttributes attrs) -> {
+                try (FileInputStream input = new FileInputStream(file.toFile())) {
+                    ZipEntry zipEntry = new ZipEntry(directory.relativize(file).toString());
+                    zipOut.putNextEntry(zipEntry);
+                    byte[] buf = new byte[ZIP_BUFSIZE];
+                    int chunkLen;
+                    while ((chunkLen = input.read(buf)) >= 0) {
+                        zipOut.write(buf, 0, chunkLen);
+                    }
+                }
+                return FileVisitResult.CONTINUE;
+            });
+        }
+
 
     }
 
-    public static void delete(@NotNull File folder) {
-        Stack<@NotNull File> stack = new Stack<>();
-        stack.add(folder);
-
-        while (!stack.isEmpty()) {
-            // TODO: symlinks?
-            File next = stack.peek();
-            File[] children = next.listFiles();
-
-            if (next.isFile() || (next.isDirectory() && children != null && children.length == 0)) { // file or empty folder
-                try {
-                    Files.deleteIfExists(next.toPath());
-                } catch (IOException e) {
-                    throw new RuntimeException("this should never, ever happen!");
-                }
-                stack.pop();
-            } else if (children != null) { // non-empty folder
-                stack.addAll(Arrays.asList(children));
-            } else {
-                stack.pop();
-            }
+    @FunctionalInterface
+    private interface SimpleFileVisitor extends FileVisitor<Path> {
+        @Override
+        default FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+            return FileVisitResult.CONTINUE;
         }
 
+        @Override
+        FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException;
+
+        @Override
+        default FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        default FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
